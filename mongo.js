0c0ce5e7dc8f5da57fa04561925366e8c1f15c0f
Cuong Dang <70055475+Amecara@users.noreply.github.com.>
2025-03-09 15:22:27 -0400

    Finish ex 3.12

--- mongo.js
+++ mongo.js
@@ -1,33 +1,46 @@
 const mongoose = require("mongoose");
-require('dotenv').config();
+require("dotenv").config();
 
 const username = encodeURIComponent(process.env.MONGODB_USERNAME);
 const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
 
 // if appName = '', will be saved in folder 'test'
 // else will be saved in folder 'appName'
-const appName = '';
+const appName = "phonebook";
 
 const url = `mongodb+srv://${username}:${password}@cluster0.fbmd6.mongodb.net/${appName}?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose.set("strictQuery", false);
-
 mongoose.connect(url);
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 });
 
-const Note = mongoose.model("Note", noteSchema);
+const Person = mongoose.model("Person", personSchema);
 
-const note = new Note({
-  content: "HTML is easy",
-  important: true,
+if (process.argv.length < 4) {
+  console.log("missing name/number");
+  process.exit(1);
+}
+
+const person = new Person({
+  name: process.argv[2],
+  number: process.argv[3],
 });
 
-note.save().then((result) => {
-  console.log(result);
-  console.log("note saved!");
-  mongoose.connection.close();
-});
+person
+  .save()
+  .then((res) => {
+    console.log("person saved to database");
+    return Person.find({});
+  })
+  .then((res) => {
+    res.forEach((person) => console.log(person));
+    mongoose.connection.close();
+  })
+  .catch((err) => {
+    console.log(err);
+    mongoose.connection.close();
+  });
