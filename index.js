e54f93f4bea1ccf4dec75ae4cba7826b1b580d21
Cuong Dang <70055475+Amecara@users.noreply.github.com.>
2025-03-09 00:37:48 -0500

    Update port to process.env.PORT || 3001

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 morgan.token("body", (req) => JSON.stringify(req.body));
 app.use(
@@ -95,7 +97,7 @@ app.post("/api/persons/", (req, res) => {
   res.json(persons);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
